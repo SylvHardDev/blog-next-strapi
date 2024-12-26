917b119a4f9b32e8b700f0d1f936c13dd2edfcdb
SylvHardDev <hardsyldev@gmail.com>
2024-12-26 16:59:07 +0300

    create component blogs

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Blogs = () => {
   return (
-    <div>Blogs</div>
-  )
-}
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <div></div>
+    </div>
+  );
+};
 
-export default Blogs
+export default Blogs;
