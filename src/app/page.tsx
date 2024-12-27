0b5c019850750ec12470fdb2a8ae09b627217904
SylvHardDev <hardsyldev@gmail.com>
2024-12-27 10:58:46 +0300

    function fetchCategories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
 import Blogs from "../components/Blogs";
 import Categories from "../components/Categories";
 
-export default function Home() {
+async function fetchCategories() {
+  const options = {
+    header: {
+      Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
+    },
+  };
+
+  try {
+    const res = await fetch("http://127.0.0.1:1337/categories", options);
+    const response = await res.json()
+    return response
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export default async function Home() {
+
+  const categories = await  fetchCategories()
+
   return (
     <div>
-      <Categories />
+      <Categories categories = {categories} />
       <Blogs />
     </div>
   );
