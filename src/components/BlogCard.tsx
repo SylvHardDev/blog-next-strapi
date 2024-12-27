726d7918ab1852d9cec1fc1aa381ab5da7e31043
SylvHardDev <hardsyldev@gmail.com>
2024-12-27 08:34:40 +0300

    fix style remove div

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,18 +1,30 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const BlogCard = () => {
   return (
-    <div>
+    <div className="rounded-lg shadow-md p-4 mb-4 overflow-hidden border border-gray-600 cursor-pointer">
       <Link href={"/"}>
         <div>
-          <Image layout='fill' objectFit='cover' src={""} alt='' className='rounded-t-lg'/>
+          <Image
+            layout="fill"
+            objectFit="cover"
+            src={""}
+            alt=""
+            className="rounded-t-lg"
+          />
+        </div>
 
+        <div className="p-2">
+          <h2 className="text-xl font-semibold mb-2 overflow-ell">
+            Title of Blog
+          </h2>
+          <p className="text-gray-600">This is the description</p>
         </div>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default BlogCard
+export default BlogCard;
