d8c2944e1d1fa29d725c3f58c47353a08d9c2ca5
SylvHardDev <hardsyldev@gmail.com>
2024-12-27 08:34:57 +0300

    fix style remove div

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import BlogCard from "./BlogCard";
 
 const Blogs = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-      <div></div>
+      <div>
+        <BlogCard />
+      </div>
+      <div>
+        <BlogCard />
+      </div>
+      <div>
+        <BlogCard />
+      </div>
     </div>
   );
 };
