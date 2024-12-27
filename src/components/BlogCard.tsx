8530e256ce963776deb03e4321999f93dab86ef4
SylvHardDev <hardsyldev@gmail.com>
2024-12-27 08:15:31 +0300

    add imge and link in blogCard

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const BlogCard = () => {
   return (
-    <div>BlogCard</div>
+    <div>
+      <Link href={"/"}>
+        <div>
+          <Image layout='fill' objectFit='cover' src={""} alt='' className='rounded-t-lg'/>
+
+        </div>
+      </Link>
+    </div>
   )
 }
 
