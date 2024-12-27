08c42bfdaa5b6db1f1367f717360f1be45ce3f6d
SylvHardDev <hardsyldev@gmail.com>
2024-12-27 08:35:39 +0300

    import category

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -4,12 +4,10 @@ import Category from "./Category";
 const Categories = () => {
   return (
     <div className="flex gap-6 mb-8">
-      <div>
-        <Category />
-        <Category />
-        <Category />
-        <Category />
-      </div>
+      <Category />
+      <Category />
+      <Category />
+      <Category />
     </div>
   );
 };
