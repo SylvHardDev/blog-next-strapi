5a0665682c6f7e620391fb11974cdae42e81414c
SylvHardDev <hardsyldev@gmail.com>
2024-12-27 08:34:17 +0300

    create page blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import Blogs from "../components/Blogs";
 import Categories from "../components/Categories";
 
 export default function Home() {
   return (
-    <div className="flex gap-6 mb-8">
-      <div>
-        <Categories />
-      </div>
+    <div>
+      <Categories />
+      <Blogs />
     </div>
   );
 }
